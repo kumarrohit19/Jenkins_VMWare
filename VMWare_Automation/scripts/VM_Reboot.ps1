ed76a9821cbf69128cc0feaec674a370635cb9fd
Rohit Kumar <kumar.rohit19@outlook.com>
2026-03-12 11:14:09 +0530

    Add files via upload

---
+++ VMWare_Automation/scripts/VM_Reboot.ps1
@@ -0,0 +1,16 @@
+﻿$vcenter = "indprdvc01.mylab.local"
+
+Connect-VIServer $vcenter
+
+$vms = Get-Content "D:\VMware-Automation\vm_lists\vm_list.txt"
+
+foreach ($vm in $vms)
+{
+    try {
+        Restart-VMGuest -VM $vm -Confirm:$false
+        Write-Output "$vm reboot initiated"
+    }
+    catch {
+        Write-Output "$vm failed"
+    }
+}
---
+++ VMWare_Automation/scripts/VM_Snapshot.ps1
@@ -0,0 +1,50 @@
+﻿param(
+    [string]$vcenter,
+    [string]$vmList,
+    [string]$action,
+    [string]$snapshotName
+)
+
+#Import-Module VMware.PowerCLI
+
+$vcUser = $env:VC_USER
+$vcPass = $env:VC_PASS
+
+$securePass = ConvertTo-SecureString $vcPass -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential ($vcUser,$securePass)
+
+Connect-VIServer $vcenter
+
+$vms = $vmList -split "`n"
+
+foreach ($vm in $vms) {
+
+    $vm = $vm.Trim()
+
+    if ($vm -eq "") { continue }
+
+    Write-Host "Processing VM: $vm"
+
+    if ($action -eq "CREATE") {
+
+        New-Snapshot -VM $vm `
+        -Name $snapshotName `
+        -Description "Snapshot created from Jenkins" `
+        -Memory:$false `
+        -Quiesce:$true
+
+        Write-Host "Snapshot created on $vm"
+
+    }
+
+    elseif ($action -eq "DELETE") {
+
+        Get-Snapshot -VM $vm -Name $snapshotName | Remove-Snapshot -Confirm:$false
+
+        Write-Host "Snapshot deleted on $vm"
+
+    }
+
+}
+
+Disconnect-VIServer -Confirm:$false
